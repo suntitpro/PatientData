29e2dbd498f38bb45f490cf6f61fe4481bc6a204
Uzzi <Uzzi@UZZI-PC>
2017-04-07 17:11:15 +0300

    Add Patient class

---
+++ PatientData/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace PatientData.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public ICollection<Ailment> Ailments { get; set; }
+        public ICollection<Medication> Medications { get; set; }
+    }
+
+    public class Medication
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Doses { get; set; }
+        
+    }
+
+    public class Ailment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
