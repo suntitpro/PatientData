80919f6a5ad2773c1cd74ee4ea0c781e23cd8105
denya_bit <denyabit@gmail.com>
2017-04-08 12:24:27 +0300

    Add old way to query by ID

--- PatientData/Controllers/PatientsController.cs
+++ PatientData/Controllers/PatientsController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using PatientData.Models;
 
@@ -12,6 +14,15 @@ namespace PatientData.Controllers
             using (var db = new ApplicationDbContext())
             {
                 return db.Patients.Include("Ailments").Include("Medications").ToList();
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var patient = db.Patients.Include("Ailments").Include("Medications").First(i => i.Id == id);
+                return patient == null ? Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found") : Request.CreateResponse(patient);
             }
         }
     }
